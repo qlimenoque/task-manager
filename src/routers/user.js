a5c0e751ce5436fd4c57cacbf32c52035d01d471
vklimenok <vladyslav.klymenok@jevera.software>
2020-09-17 21:24:54 +0300

    Add upload/delete profile avatar

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = new express.Router();
+const multer = require('multer');
 const User = require('../models/user');
 const auth = require('../middleware/auth');
 
@@ -78,5 +79,34 @@ router.delete('/users/me', auth, async (req, res) => {
         res.status(500).send(e);
     }
 });
+
+const upload = multer({
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error('Please, upload images only'));
+        }
+        cb(undefined, true);
+    }
+})
+
+// Avatar uploading
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+    req.user.avatar = req.file.buffer;
+    await req.user.save();
+    res.send();
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
+})
+
+// Avatar deleting
+
+router.delete('/users/me/avatar', auth, async (req, res) => {
+    req.user.avatar = undefined;
+    await req.user.save();
+    res.send();
+})
 
 module.exports = router;
